3f3e7ed2755e59e0aa1a5491094302056a584e1c
Uriel González <ugonzalezpy@gmail.com>
2013-10-30 15:41:09 -0300

    Add the Alfabeto class.
    The Alfabeto class represents an alphabet.

---
+++ src/main/java/py/una/compilation/Alfabeto.java
@@ -0,0 +1,102 @@
+package py.una.compilation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase que representa un alfabeto.
+ * 
+ * @author Uriel Gonzalez
+ * 
+ */
+public class Alfabeto {
+
+	private String nombre;
+	private final List<Simbolo> simbolos;
+
+	/**
+	 * Construye un alfabeto.
+	 */
+	public Alfabeto() {
+		this.simbolos = new ArrayList<Simbolo>();
+	}
+
+	/**
+	 * Setea el nombre del alfabeto.
+	 * 
+	 * @param nombre
+	 *            Nuevo nombre del alfabeto.
+	 */
+	public void setNombre(final String nombre) {
+		this.nombre = nombre.toUpperCase();
+	}
+
+	/**
+	 * Recupera el nombre del alfabeto.
+	 * 
+	 * @return El nombre del alfabeto.
+	 */
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	/**
+	 * Agrega un nuevo simbolo al alfabeto.
+	 * 
+	 * @param simbolo
+	 *            Nuevo simbolo del alfabeto.
+	 */
+	public void addSimbolo(final Simbolo simbolo) {
+		this.simbolos.add(simbolo);
+
+	}
+
+	/**
+	 * Recupera la lista de simbolos del alfabeto.
+	 * 
+	 * @return Los simbolos del alfabeto.
+	 */
+	public List<Simbolo> getSimbolos() {
+		return this.simbolos;
+	}
+
+	/**
+	 * Recupera un simbolo del alfabeto a partir de su etiqueta.
+	 * 
+	 * @param etiqueta
+	 *            Etiqueta del simbolo a recuperar.
+	 * @return Simbolo del alfabeto al cual pertenece la etiqueta pasada como
+	 *         parametro. En caso de que no exista un simbolo en el alfabeto con
+	 *         la etiqueta pasada como parametro retorna null.
+	 */
+	public Simbolo getSimbolo(final String etiqueta) {
+		final Simbolo simboloBuscado = new Simbolo(etiqueta);
+		for (final Simbolo simbolo : this.simbolos) {
+			if (simbolo.equals(simboloBuscado)) {
+				return simbolo;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * 
+	 * @param simbolo
+	 * @return true en caso que el simbolo se encuentre en el alfabeto
+	 */
+	public boolean hasSimbolo(final Simbolo simbolo) {
+		return this.simbolos.contains(simbolo);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder();
+		sb.append(this.nombre + " = { ");
+		for (final Simbolo simbolo : this.simbolos) {
+			sb.append(simbolo + " ");
+		}
+		sb.append("}");
+		return sb.toString();
+	}
+
+}
