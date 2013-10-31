0ca1773b609a5302d3c2448147b01b307b76b520
Uriel González <ugonzalezpy@gmail.com>
2013-10-31 19:25:07 -0300

    Add the exception ErrorSintaxis.
    The ErrorSintaxis exception must be launched when a regular expression is bad
    formed.

---
+++ src/main/java/py/una/compilation/ErrorSintaxis.java
@@ -0,0 +1,19 @@
+package py.una.compilation;
+
+/**
+ * Error a ser lanzado cuando la expresión regular no esta bien formada.
+ * 
+ * @author Uriel González
+ * 
+ */
+@SuppressWarnings("serial")
+public class ErrorSintaxis extends Exception {
+
+	/**
+	 * Construye la excepción ErrorSintaxis.
+	 */
+	public ErrorSintaxis() {
+		super();
+	}
+
+}
